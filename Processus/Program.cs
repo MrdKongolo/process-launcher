5d2d23ac2645ec0ce1d8fdc097428f6737d3c9ee
MrdKongolo <medardbaraka09@gmail.com>
2022-11-28 17:04:07 +0200

    first commits

---
+++ Processus/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p;
+            Console.WriteLine("Entrez le nom de votre programme à lancer :");
+            try
+            {
+                string prog = Console.ReadLine();
+                p= Process.Start(string.Format($"{prog}"));
+                Console.WriteLine("Appuyez sur une touche pour mettre fin à ce processus.");
+                Console.ReadKey();
+                p.Kill();                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+            Console.ReadKey();
+        }
+    }
+}
